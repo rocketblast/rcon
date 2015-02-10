334eef66dcd7789120ec232aa4fa0e89cc73c5f8
Edvin Erikson <edvin@rocketblast.com>
2015-02-10 17:17:29 +0100

    changed setup.py to follow PEP8 guidelines

--- setup.py
+++ setup.py
@@ -10,27 +10,27 @@ datadir = os.path.join('data')
 data_files = [(datadir, [f for f in glob.glob(os.path.join(datadir, '*'))])]
 
 setup(
-    name             = 'Rocket Blast RCON',
-    version          = '0.1.1',
-#    maintainer       = '',
-#    maintainer_email = '',
-    author           = 'Martin Danielson',
-    author_email     = 'martin@rocketblast.com',
-    long_description = open("README.md").read(),
-    keywords         = 'rcon battlefield game servers socket connection',
-    description      = 'Library to connect to game servers over rcon protcol (Battlefield)',
-    license          = 'GNU Affero GPL v3',
-#    platforms        = '',
-    url              = 'https://github.com/rocketblast/rcon',
-    download_url     = 'https://github.com/rocketblast/rcon/downloads',
-    classifiers      = '',
+    name='Rocket Blast RCON',
+    version='0.1.1',
+    # maintainer       = '',
+    # maintainer_email = '',
+    author='Martin Danielson',
+    author_email='martin@rocketblast.com',
+    long_description=open("README.md").read(),
+    keywords='rcon battlefield game servers socket connection',
+    description='Library to connect to game servers over rcon protcol (Battlefield)',
+    license='GNU Affero GPL v3',
+    # platforms='',
+    url='https://github.com/rocketblast/rcon',
+    download_url='https://github.com/rocketblast/rcon/downloads',
+    classifiers='',
 
     # package installation
-    package_dir = package_dir,
-    packages = find_packages('src'),
-    namespace_packages = ['rocketblast', 'rocketblast.rcon'],
+    package_dir=package_dir,
+    packages=find_packages('src'),
+    namespace_packages=['rocketblast', 'rocketblast.rcon'],
 
-    install_requires = [],
+    install_requires=[],
     # uncomment if you have share/data files
     #data_files = data_files,
 
