5be1e56e116373aa688e6272a0c3ed33f4bd5a52
Edvin Wennerdahl Eriksson <edvin@pokeyo.se>
2023-04-27 20:48:49 +0200

    separate package names

--- rocketblast/rcon/__init__.py
+++ rocketblast/rcon/__init__.py
@@ -1,5 +1,2 @@
-__import__('pkg_resources').declare_namespace(__name__)
 from .frostbite.frostbite import Client as FrostbiteClient
 from .frostbite.frostbite import FormatClient as FormatFrostbiteClient
-
-__all__ = ['frostbite']
